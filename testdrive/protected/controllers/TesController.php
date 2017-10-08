6027bb2417043fc42fec1b276c8fd5bcf28591fa
sidiqagus.as <sidiqagus.as@gmail.com>
2017-10-08 10:34:23 +0700

    commit ke dua

---
+++ testdrive/protected/controllers/TesController.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * 
+ */
+class TesController extends Controller
+{
+    public function actionIndex()
+    {
+        // $this->render('index');
+        $testing = 'buntingbro?';
+ 		$this->render('index',array('testing'=>$testing));
+    }
+
+	// -----------------------------------------------------------
+	// Uncomment the following methods and override them if needed
+	/*
+	public function filters()
+	{
+		// return the filter configuration for this controller, e.g.:
+		return array(
+			'inlineFilterName',
+			array(
+				'class'=>'path.to.FilterClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+
+	public function actions()
+	{
+		// return external action classes, e.g.:
+		return array(
+			'action1'=>'path.to.ActionClass',
+			'action2'=>array(
+				'class'=>'path.to.AnotherActionClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+	*/
+}
